9f83fc5b191dc4544d4b9cb78871e92d29e830d3
Ed Ignot <edignot@gmail.com>
2020-06-04 21:16:32 -0600

    Linking to main page

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <button>BUCKET</button>
+        </nav>
+    )
+}
+
+export default Nav
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import { Route, Redirect, Switch } from 'react-router-dom'
 import { withRouter } from 'react-router';
 import Login from './Login'
 import NotFound from '../components/NotFound'
+import NPContainer from './NPContainer'
+import Nav from '../components/Nav'
 
 class App extends Component {
   render() {
@@ -12,20 +14,26 @@ class App extends Component {
       <section className="app-container">
         <h1>National Parks Bucket List</h1>
 
-        {!user.username && <Redirect to="/" />}
+        {!user.username && <Redirect to="/login" />}
 
         <Switch>
-          <Route exact path = '/' component={Login}/>
+          <Route exact path = '/login' component={Login}/>
+
+          <Route exact path = '/'>
+              <NPContainer/>
+              <Nav/>
+          </Route>
+
           <Route component={NotFound} />
         </Switch>
 
       </section>
-    );
-  }
+    )
+  } 
 }
 
 export const mapStateToProps = (state) => ({
   user: state.user
 })
 
-export default connect(mapStateToProps,null)(withRouter(App));
+export default connect(mapStateToProps, null)(withRouter(App));
