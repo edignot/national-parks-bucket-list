73f3ca993681c5cd4febaf818c6ba19e575b26fc
Ed Ignot <edignot@gmail.com>
2020-06-04 22:52:45 -0600

    Nav component links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { BsBucketFill, BsFillPersonFill } from 'react-icons/bs'
 import { FaCheckCircle, FaSearch, FaMapMarkedAlt } from 'react-icons/fa'
 
 const Nav = () => {
     return (
         <nav>
-            <FaSearch/>
-            <BsBucketFill/>
-            <FaCheckCircle/>
-            <FaMapMarkedAlt/>
-            <BsFillPersonFill/>
+            <Link to='explore' className='nav-item'>
+                <FaSearch/>
+                EXPLORE
+            </Link>
+            <Link to='bucketlist' className='nav-item'>
+                <BsBucketFill/>
+                BUCKET LIST
+            </Link>
+            <Link to='visited' className='nav-item'>
+                <FaCheckCircle/>
+                VISITED
+            </Link>
+            <Link to='map' className='nav-item'>
+                <FaMapMarkedAlt/>
+                MAP
+            </Link>
+            <Link to='userpage' className='nav-item'>
+                <BsFillPersonFill/>
+                USERPAGE
+            </Link>
         </nav>
     )
 }
