59a9999e04e5b4bac9ab35dccd58d828d8acfb1e
Ed Ignot <edignot@gmail.com>
2020-06-06 10:58:02 -0600

    Map display coordinates

---
+++ src/components/MapTag.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MapTag = (props) => {
+    return (
+      <div>
+        <h1>HELLOO</h1>
+      </div>
+    )
+  }
--- src/containers/Map.js
+++ src/containers/Map.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { connect } from 'react-redux'
+import { MapTag } from '../components/MapTag'
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
 
 
 const Map = ({np}) => {
     console.log(np)
-    // const otherSpots = GlobalStore.spots.filter(spot => spot.id !== GlobalStore.spotDetails.id)
-    // const markers = otherSpots.map(spot => {
-    //   const marker = [{...spot.coordinates}]
-    //   return (
-    //     <MapTag
-    //           lat={marker[0].lat}
-    //           lng={marker[0].lng}
-    //           id={spot.id}
-    //           name={spot.name}
-    //         />
-    //   )
-    // })
-    // const targetMarker = [{...GlobalStore.spotDetails.coordinates}]
+    const markers = np.map(park => {
+      return (
+        <MapTag
+              lat={park.latitude}
+              lng={park.longitude}
+              id={park.id}
+              name={park.name}
+            />
+      )
+    })
       return (
         <div style={{ height: '300px', width: '300px' }}>
           <GoogleMapReact
@@ -26,8 +24,8 @@ const Map = ({np}) => {
             center={{lat: 59.95, lng: 30.33}}
             zoom={16}
           >
-            {/* { markers }
-            <MapTargetTag
+            { markers }
+            {/* <MapTargetTag
               lat={targetMarker[0].lat}
               lng={targetMarker[0].lng}
               mapUrl={GlobalStore.spotDetails.mapUrl}
